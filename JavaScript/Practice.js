cf1d76c9a14d8543362890b2ca37d4e595dd9d24
sangram <sangram202512@gmail.com>
2026-05-12 17:19:53 +0530

    js practice

---
+++ JavaScript/Practice.js
@@ -0,0 +1,28 @@
+var x = 5;
+let y = 10;
+const z = 15;
+var q = x+y+z
+console.log(x, y, z)
+console.log("q = " + q)
+
+
+let d = 23
+console.log(d)
+ d = 33
+console.log("d = " + d)
+
+if (true) {
+    let p = 25
+    const u = 63
+
+    console.log("inside value " + p)
+    console.log("inside value " + u)
+}
+
+//console.log("out side vlue " + p)
+
+
+const name = ["shweta", "shivani", "shivaji"]
+name[0] = "shweta patil"
+name.push("shivaji patil")
+console.log(name)
